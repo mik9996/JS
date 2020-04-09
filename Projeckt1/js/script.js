ea2f44b6b5c0835d004a037dc6540d840f8b20ad
Mik <zhutov24@gmail.com>
2020-04-09 13:27:49 +0400

    Первый коммит

---
+++ Projeckt1/js/script.js
@@ -0,0 +1,21 @@
+
+var money = "Ваш бюджет?";
+var nameScore = "Название вашего магазина?";
+var mainList = {
+	maneyScore: money,
+	shopName: nameScore,
+	shopGoods: [],
+	employers: {},
+	open: false
+}
+//let question = prompt('Какой тип товаров будем продавать?', "");
+//console.log(question);
+//alert( mainList.maneyScore / 30 );
+//console.log( mainList.maneyScore );
+
+/*Второе задание*/
+
+let num = 33721;
+num = (3 * 3 * 7 * 2 * 1);
+num = (num ** 2);
+console.log(num);
